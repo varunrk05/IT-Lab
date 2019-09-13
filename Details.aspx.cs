2cc0ea612f98ff42b9b3a29ceccfb249f852336a
Varun <36574020+varunrk05@users.noreply.github.com>
2019-09-13 13:55:45 +0530

    Lab 5 question 1 added

---
+++ Details.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace question1
+{
+    public partial class Details : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblManuf.Text = "";
+                lblModel.Text = "";
+            }
+
+            string manuf = Request.QueryString["Manufac"];//Server.UrlDecode("Manufac");
+            string model = Request.QueryString["Model"];//Server.UrlDecode("Model");
+            lblManuf.Text = "The manufacturer is: " + manuf;
+            lblModel.Text = "The model is: " + model;
+        }
+    }
+}
